9c55bddb177fa3a88c19db5d3985fbf094ef1436
dimon <dimon20.07@mail.ru>
2023-11-01 17:00:07 +0300

    .\AgeComporator.java

---
+++ AgeComporator.java
@@ -0,0 +1,20 @@
+package org.example;
+
+import java.util.Comparator;
+
+public class AgeComparator implements Comparator<Employee> {
+
+    @Override
+    public int compare(Employee emp1, Employee emp2) {
+        int age1 = (int) emp1.getAge();
+        int age2 = (int) emp2.getAge();
+
+        if (age1 < age2) {
+            return -1;
+        } else if (age1 > age2) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+}
